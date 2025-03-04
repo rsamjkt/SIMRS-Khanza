d037ab468fa154c00ad3cef777f6b236bdac63c0
randy mandala <randy@rsanggrekmas.com>
2025-03-04 10:28:14 +0700

    update fix yang skrining diabetes

--- src/fungsi/koneksiDB.java
+++ src/fungsi/koneksiDB.java
@@ -31,8 +31,8 @@ public class koneksiDB {
                 dataSource.setUser(EnkripsiAES.decrypt(prop.getProperty("USER")));
                 dataSource.setPassword(EnkripsiAES.decrypt(prop.getProperty("PAS")));
                 dataSource.setCachePreparedStatements(true);
-                dataSource.setPreparedStatementCacheSize(500);          
-                dataSource.setPreparedStatementCacheSqlLimit(4096); 
+                dataSource.setPreparedStatementCacheSize(100);    
+                dataSource.setPreparedStatementCacheSqlLimit(2048);
                 dataSource.setUseCompression(true);
                 dataSource.setAutoReconnect(true);
                 dataSource.setAutoReconnectForPools(true);
@@ -41,8 +41,8 @@ public class koneksiDB {
                 dataSource.setUseLocalSessionState(true);               
                 dataSource.setUseLocalTransactionState(true); 
                 dataSource.setLoginTimeout(10);                         
-                dataSource.setConnectTimeout(10000);                  
-                dataSource.setSocketTimeout(30000);  
+                dataSource.setConnectTimeout(30000);                  
+                dataSource.setSocketTimeout(90000);  
                 connection=dataSource.getConnection();       
                 System.out.println("  Koneksi Berhasil. Sorry bro loading, silahkan baca dulu.... \n\n"+
                         "Jika Ada Kendala Silakan Hubungi IT \n"+
@@ -57,8 +57,8 @@ public class koneksiDB {
                         dataSource.setUser(EnkripsiAES.decrypt(prop.getProperty("USER")));
                         dataSource.setPassword(EnkripsiAES.decrypt(prop.getProperty("PAS")));
                         dataSource.setCachePreparedStatements(true);
-                        dataSource.setPreparedStatementCacheSize(500);          
-                        dataSource.setPreparedStatementCacheSqlLimit(4096); 
+                        dataSource.setPreparedStatementCacheSize(100);          
+                        dataSource.setPreparedStatementCacheSqlLimit(2048); 
                         dataSource.setUseCompression(true);
                         dataSource.setAutoReconnect(true);
                         dataSource.setAutoReconnectForPools(true);
@@ -67,8 +67,8 @@ public class koneksiDB {
                         dataSource.setUseLocalSessionState(true);               
                         dataSource.setUseLocalTransactionState(true); 
                         dataSource.setLoginTimeout(10);                         
-                        dataSource.setConnectTimeout(10000);                  
-                        dataSource.setSocketTimeout(30000);
+                        dataSource.setConnectTimeout(30000);                  
+                        dataSource.setSocketTimeout(90000); 
                         connection=dataSource.getConnection();  
                     }
                 } catch (Exception ex) {
